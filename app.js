c7d31187d5e063eea8764129cbf13c252add4b63
MorvidAngel <5d7n2s9m6b@live.com>
2022-07-27 22:14:30 -0400

    starting js

--- app.js
+++ app.js
@@ -1,1 +1,54 @@
 // log to console
+console.log(
+    'Hello World'
+);
+
+console.log(123);
+
+console.log(true);
+
+var greeting = 'Hello';
+console.log(greeting);
+
+console.log([1, 2, 3, 4]);
+
+console.log({ a: 1, b: 2 });
+
+console.table({ a: 1, b: 2 });
+
+console.error('This is some error');
+
+console.clear();
+
+console.warn('This is a warning');
+
+console.time('This is going to tell me how long hello world took');
+
+console.log('Hello World');
+console.log('Hello World');
+console.log('Hello World');
+console.log('Hello World');
+console.log('Hello World');
+console.log('Hello World');
+console.log('Hello World');
+console.log('Hello World');
+console.log('Hello World');
+console.log('Hello World');
+console.log('Hello World');
+console.log('Hello World');
+console.log('Hello World');
+console.log('Hello World');
+console.log('Hello World');
+console.log('Hello World');
+console.log('Hello World');
+console.log('Hello World');
+console.log('Hello World');
+console.log('Hello World');
+
+console.timeEnd('This is going to tell me how long hello world took');
+
+/*
+multiple 
+line 
+comments
+*/ 
