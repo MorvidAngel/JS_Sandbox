107bac43b19eead5cc41282ca4b50423ed7bca1f
MorvidAngel <5d7n2s9m6b@live.com>
2022-08-02 01:06:20 -0400

    data types

--- app.js
+++ app.js
@@ -1,76 +1,132 @@
-// log to console
-console.log(
-    'Hello World'
-);
+// ! log to console
+// console.log(
+//     'Hello World'
+// );
 
-console.log(123);
+// console.log(123);
 
-console.log(true);
+// console.log(true);
 
-var greeting = 'Hello';
-console.log(greeting);
+// var greeting = 'Hello';
+// console.log(greeting);
 
-console.log([1, 2, 3, 4]);
+// console.log([1, 2, 3, 4]);
 
-console.log({ a: 1, b: 2 });
+// console.log({ a: 1, b: 2 });
 
-console.table({ a: 1, b: 2 });
+// console.table({ a: 1, b: 2 });
 
-console.error('This is some error');
+// console.error('This is some error');
 
-console.clear();
+// console.clear();
 
-console.warn('This is a warning');
+// console.warn('This is a warning');
 
-console.time('This is going to tell me how long hello world took');
+// console.time('This is going to tell me how long hello world took');
 
-console.log('Hello World');
-console.log('Hello World');
-console.log('Hello World');
-console.log('Hello World');
-console.log('Hello World');
-console.log('Hello World');
-console.log('Hello World');
-console.log('Hello World');
-console.log('Hello World');
-console.log('Hello World');
-console.log('Hello World');
-console.log('Hello World');
-console.log('Hello World');
-console.log('Hello World');
-console.log('Hello World');
-console.log('Hello World');
-console.log('Hello World');
-console.log('Hello World');
-console.log('Hello World');
-console.log('Hello World');
+// console.log('Hello World');
+// console.log('Hello World');
+// console.log('Hello World');
+// console.log('Hello World');
+// console.log('Hello World');
+// console.log('Hello World');
+// console.log('Hello World');
+// console.log('Hello World');
+// console.log('Hello World');
+// console.log('Hello World');
+// console.log('Hello World');
+// console.log('Hello World');
+// console.log('Hello World');
+// console.log('Hello World');
+// console.log('Hello World');
+// console.log('Hello World');
+// console.log('Hello World');
+// console.log('Hello World');
+// console.log('Hello World');
+// console.log('Hello World');
 
-console.timeEnd('This is going to tell me how long hello world took');
+// console.timeEnd('This is going to tell me how long hello world took');
 
-/*
-multiple 
-line 
-comments
-*/
+//
+// multiple
+// line
+// comments
+//
 
-// var, let, const
+// ! var, let, const
 
-var name = 'John Doe';
-console.log(name);
-name = 'Steve Smith';
-console.log(name);
+// * var
+// var name = 'John Doe';
+// console.log(name);
+// name = 'Steve Smith';
+// console.log(name);
 
-// inititiolizing a variable
-var greeting;
-console.log(greeting);
-greeting = 'Hello'
-console.log(greeting);
+//  inititiolizing a variable
+// var greeting;
+// console.log(greeting);
+// greeting = 'Hello'
+// console.log(greeting);
 
-// your variable cant start with numbers. it can consist with letters, numbers, _ , $
+//  your variable cant start with numbers. it can consist with letters, numbers, _ , $
 
-// style of wrighting variables with multiple words
-var firstName = 'John'; // this is camel case
-var fisrt_name = 'Sara'; // underscore style
-var FirstName = 'Tom'; //pascal case style
-var firstname = 'Doodle'; // not recomended
+//  style of wrighting variables with multiple words
+// var firstName = 'John'; // this is camel case
+// var fisrt_name = 'Sara'; // underscore style
+// var FirstName = 'Tom'; //pascal case style
+// var firstname = 'Doodle'; // not recomended
 
+// * let
+// let name = 'John Doe';
+// console.log(name);
+// name = 'Steve Smith';
+// console.log(name);
+
+// * const
+// const name = 'John';
+// console.log(name);
+// const cannot be reassigned and must have a value
+
+// const person = {
+//     name: 'John',
+//     age: 30
+// }
+
+// person.name = 'Sara';
+// person.age = '60';
+
+// console.log(person);
+
+// const numbers = [1, 2, 3, 4, 5];
+// numbers.push(6);
+
+// console.log(numbers);
+
+
+// !Data Types
+// ! primitive
+//  * String
+// const name = 'John Doe';
+// * Number
+// const age = 45;
+// * Boolean
+// const hasKids = true;
+// *Null
+// const car = null;
+// *Undefined
+// let test;
+// *Symbol
+// const sym = Symbol();
+
+// !Reference types - objects
+// *Array
+// const hobbies = ['anime', 'gaming', 'movies', 'music'];
+// *Object literal
+// const address = {
+//     city: 'Brooklyn',
+//     state: 'NY'
+// }
+// *Date
+// const today = new Date();
+
+// console.log(today);
+// console.log(typeof today);
