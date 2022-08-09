9bbceb4d9b5b89c115339aed48c16b1d89fd67c4
MorvidAngel <5d7n2s9m6b@live.com>
2022-08-09 03:36:41 -0400

    arrays and array methods

--- app.js
+++ app.js
@@ -204,12 +204,12 @@
 // console.log(val);
 
 // !string methods & concatenation
-const firstName = 'Morvid';
-const lastName = 'Angel';
-const age = '190';
-const str = 'Hello there my name is Morvid';
-const tags = 'web design, web Development, programming, game design';
-let val;
+// const firstName = 'Morvid';
+// const lastName = 'Angel';
+// const age = '190';
+// const str = 'Hello there my name is Morvid';
+// const tags = 'web design, web Development, programming, game design';
+// let val;
 
 // val = firstName + lastName;
 
@@ -254,13 +254,136 @@ let val;
 // val = firstName.slice(-3);
 
 // *split()
-val = str.split(' ');
-val = tags.split(',');
+// val = str.split(' ');
+// val = tags.split(',');
 
 // *replace()
-val = str.replace('Morvid', 'Rick James bitch!');
+// val = str.replace('Morvid', 'Rick James bitch!');
 
 // * includes()
-val = str.includes('foo');
+// val = str.includes('foo');
 
+// console.log(val);
+
+// !Template Literals
+// const name = 'Elmo';
+// const age = 230;
+// const job = 'Teacher';
+// const city = 'New York City';
+// let html;
+
+// function hello() {
+//     return 'hello';
+// }
+
+// * without template strings (es5) old fasion that we wont use
+// html = '<ul><li>Name: ' + name + '</li><li>Age: ' + age + ' </li><li>Job: ' + job + ' </li><li>City: ' + city + ' </li></ul>';
+
+// html = '<ul>' +
+//     '<li>Name: ' + name + '</li>' +
+//     '<li>Age: ' + age + '</li>' +
+//     '<li>Job: ' + job + '</li>' +
+//     '<li>City: ' + city + '</li>' +
+//     '</ul>';
+
+// *with template strings (es6)
+// html = `
+//     <ul>
+//     <li>Name: ${name}</li>
+//     <li>Age: ${age}</li>
+//     <li>Job: ${job}</li>
+//     <li>City: ${city}</li>
+//     <li>${2 + 2}</li>
+//     <li>${hello()}</li>
+//     <li>${age > 30 ? 'Over 30' : 'Under 30'}</li>
+//     </ul>
+//     `;
+
+// document.body.innerHTML = html;
+
+// !Arrays and Array Methods
+// types of arrays
+const numbers = [39, 10, 4, 66, 420, 28, 123, 344, 6, 20, 80, 306];
+const numbers2 = new Array(32, 53.97, 60, 134, 55, 17,);
+const fruit = ['apple', 'bannana', 'pineapple', 'orange', 'watermelon'];
+const mixed = [22, 'hello', true, undefined, null, { a: 1, b: 1 }, new Date()];
+let val;
+
+// get array lenght
+// val = numbers.length;
+
+// check its an array
+// val = Array.isArray(numbers);
+
+// fint what item is in array location
+// val = numbers[3]; //this will show what item is in the forth spot in the array
+// val = numbers[0]; //this will show what item is in the first spot of the array
+
+// inserting a item in the array
+// numbers[2] = 100; //the 4 has changed into 100 in the array
+
+// find the value of the index
+// val = numbers.indexOf(344); // this shows you what spot is the number located on in the array
+
+// *Mutating arrays
+// add an index to the end of the array
+// numbers.push(250);
+
+// add an index to the front of the array
+// numbers.unshift(120);
+
+// take an index off from the end
+// numbers.pop();
+
+// take an index off from the front
+// numbers.shift();
+
+// splice values choose a start and end of indexs you want to remove
+// numbers.splice(4, 6);
+
+// reverse the array
+// numbers.reverse();
+
+// conatinating arrays
+// val = numbers.concat(numbers2);
+
+// sort your array
+// val = fruit.sort();
+// val = numbers.sort();
+
+//* sorting numbers
+// least to highest
+// val = numbers.sort(function (x, y) {
+//     return x - y;
+// });
+
+// highest to lowest
+// val = numbers.sort(function (x, y) {
+//     return y - x;
+// });
+
+// find and index
+function over50(num) {
+    return num > 50;
+}
+
+val = numbers.find(over50);
+
+console.log(numbers);
 console.log(val);
+
+// !object literals
+
+// !dates and time
+
+// !if statments and comparison operators
+
+// !switches
+
+// !function declaratioins and expressions
+
+// !general loops
+
+// !a look at the window object
+
+// !block scope with let and const
